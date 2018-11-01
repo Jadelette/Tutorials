f01e844b0017ee277243af77a647082fe6658852
Jade Corcoran <35224466+Jadelette@users.noreply.github.com>
2018-11-01 19:43:05 +0000

    simple scanner added

--- src/ScannerClass.java
+++ src/ScannerClass.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class ScannerClass {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter an integer:");
+        int myInt = reader.nextInt();
+
+        System.out.printf("You entered %d.%n%n", myInt);
+
+        System.out.print("Enter a string: ");
+        reader.nextLine();
+        //the additional method call above is to consume the 'new line' created after hitting 'enter' after entering the int
+        String myString = reader.nextLine();
+
+        System.out.printf("You entered \"%s\".%n%n", myString);
+
+
+
+
+
+    }
+
 }
