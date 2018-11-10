0cf1dc1af0314c218d687e7781e906d23a025693
Jade Corcoran <35224466+Jadelette@users.noreply.github.com>
2018-11-10 17:52:58 +0000

    Static demo

---
+++ src/staticdemo/MyClass.java
@@ -0,0 +1,19 @@
+package staticdemo;
+
+public class MyClass {
+
+    //non-static field and method
+    public String message = "Hello, World";
+    public void displayMessage()
+    {
+        System.out.println("Message: " + message);
+    }//end of method
+
+    //static field and method
+    public static String greetings = "Good morning";
+    public static void displayGreetings()
+    {
+        System.out.println("Greeting: " + greetings);
+    }//end of method
+}//end of class
+
